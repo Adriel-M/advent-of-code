05c93c570b2d26a384feffc81a4cb0de127a62a9
Adriel Martinez <adriel@adrielm.ca>
2019-12-01 21:13:46 -0500

    some sort of faster?

--- 2019/01 - The Tyranny of the Rocket Equation/rust/src/main.rs
+++ 2019/01 - The Tyranny of the Rocket Equation/rust/src/main.rs
@@ -14,6 +14,19 @@ fn calc_fuel2(mass: i32) -> i32 {
     }
 }
 
+fn calc_fuel3(mass: i32) -> i32 {
+    let mut curr_mass = mass;
+    let mut required_fuel = 0;
+    while curr_mass > 0 {
+        let calculated_fuel = calc_fuel1(curr_mass);
+        if calculated_fuel > 0 {
+            required_fuel += calculated_fuel;
+        }
+        curr_mass = calculated_fuel;
+    }
+    return required_fuel;
+}
+
 fn main() -> Result<(), Error> {
     let path = "../input";
 
@@ -22,16 +35,20 @@ fn main() -> Result<(), Error> {
 
     let mut total_fuel1 = 0;
     let mut total_fuel2 = 0;
+    let mut total_fuel3 = 0;
     for line in buffered.lines() {
         let parsed_line = line.unwrap().parse().unwrap();
         total_fuel1 += calc_fuel1(parsed_line);
         total_fuel2 += calc_fuel2(parsed_line);
+        total_fuel3 += calc_fuel3(parsed_line);
     }
 
     println!("===== Part 1 =====");
     println!("Total Fuel: {}", total_fuel1);
+
+    assert!(total_fuel2 == total_fuel3);
     println!("===== Part 2 =====");
-    println!("Total Fuel: {}", total_fuel2);
+    println!("Total Fuel: {}", total_fuel3);
 
     Ok(())
 }
